8c1e32363c0780ed0722350274c60edcb058173a
Med D. Bouthuri <med.d.bouthuri@gmail.com>
2022-12-08 18:42:19 -0800

    popular movies & series section

--- main.js
+++ main.js
@@ -1,22 +1,31 @@
-
-
-var key = "api_key=48586718f1619baec6911ced95941d83"
-
-var baseUrl = "https://api.themoviedb.org/3/trending/movie/day?"
-
-var apiUrl = baseUrl + key
-var zz = []
 // https://image.tmdb.org/t/p/original
-fetch(apiUrl).then(res => res.json()).then(data => {
 
-    for (var i = 15; data.results.length > i; i++) {
-        zz.push(i)
+function trendingUrl(str) {
+    return `https://api.themoviedb.org/3/trending/${str}/day?api_key=48586718f1619baec6911ced95941d83`
+}
+
+async function getMovies(url) {
+    var data = await fetch(url);
+    data = await data.json();
+    for (var i = 0; data.results.length > i; i++) {
+        console.log(data.results[i])
         var imgSrc = "https://image.tmdb.org/t/p/original" + data.results[i].backdrop_path
-        $("#trending").append(`<img src =${imgSrc}>`)
+        $("#trendingMovies").append(`<img src =${imgSrc}>`)
     }
-    console.log(zz)
+}
 
-})
+async function getSeries(url) {
+    var data = await fetch(url);
+    data = await data.json();
+    for (var i = 0; data.results.length > i; i++) {
+        console.log(data.results[i])
+        var imgSrc = "https://image.tmdb.org/t/p/original" + data.results[i].backdrop_path
+        $("#trendingSeries").append(`<img src =${imgSrc}>`)
+    }
+}
+
+getMovies(trendingUrl('movie'))
+getSeries(trendingUrl('tv'))
 
 
 
